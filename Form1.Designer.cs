3727361dc7b7efe2b38976d98aaef121f25dd776
Richard Santana <richard.santana@bitsboltsandvolts.com>
2018-11-26 20:41:46 -0800

    Importing project

---
+++ Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace LED_Control
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.portBox = new System.Windows.Forms.ComboBox();
+            this.onBox = new System.Windows.Forms.Button();
+            this.offBox = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // portBox
+            // 
+            this.portBox.FormattingEnabled = true;
+            this.portBox.Location = new System.Drawing.Point(13, 12);
+            this.portBox.Name = "portBox";
+            this.portBox.Size = new System.Drawing.Size(121, 24);
+            this.portBox.TabIndex = 0;
+            this.portBox.SelectedIndexChanged += new System.EventHandler(this.portBox_SelectedIndexChanged);
+            // 
+            // onBox
+            // 
+            this.onBox.Location = new System.Drawing.Point(13, 42);
+            this.onBox.Name = "onBox";
+            this.onBox.Size = new System.Drawing.Size(121, 33);
+            this.onBox.TabIndex = 1;
+            this.onBox.Text = "ON";
+            this.onBox.UseVisualStyleBackColor = true;
+            this.onBox.Click += new System.EventHandler(this.onBox_Click);
+            // 
+            // offBox
+            // 
+            this.offBox.Location = new System.Drawing.Point(13, 81);
+            this.offBox.Name = "offBox";
+            this.offBox.Size = new System.Drawing.Size(121, 38);
+            this.offBox.TabIndex = 2;
+            this.offBox.Text = "OFF";
+            this.offBox.UseVisualStyleBackColor = true;
+            this.offBox.Click += new System.EventHandler(this.offBox_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(148, 135);
+            this.Controls.Add(this.offBox);
+            this.Controls.Add(this.onBox);
+            this.Controls.Add(this.portBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox portBox;
+        private System.Windows.Forms.Button onBox;
+        private System.Windows.Forms.Button offBox;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace LED_Control
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try // A try block is used to encapsulate a region of code. If any code throws an exception within that try block, the exception will be handled by the corresponding catch.
+            {
+                portBox.Items.AddRange(SerialPort.GetPortNames());
+                portBox.SelectedIndex = 0; //Depending on which COM port the user selects it will put it into the 0 position in the combobox
+                serialPort1.BaudRate = (9600); //Set baudrate to 9600 to match the Arduino Nano
+                serialPort1.ReadTimeout = (2000); //2 seconds before it times out and it errors
+                serialPort1.WriteTimeout = (2000); //2 seconds before it times out and it errors
+            }
+            catch (Exception ex) //When an exception occurs, the Catch block of code is executed. This is where you are able to handle the exception, log it, or ignore it.
+            {
+                MessageBox.Show(ex.Message); //there are nothing to run here since all we did above is set the baudrate, timeouts, index, and get the COM ports available.
+            }
+        }
+
+        private void onBox_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.Open(); //Open COM port 
+                serialPort1.WriteLine("on"); //Send "on" through serial communication
+                serialPort1.Close(); //Close COM port
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void offBox_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.Open(); //Open COM port 
+                serialPort1.WriteLine("off"); //Send "off" through serial communication
+                serialPort1.Close(); //Close COM port 
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void portBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = portBox.Text; //Set the serial port name equals to the text inside portBox
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
